cd1ef0faf446199d38bf39eb5fdeee8a0c4a52b7
Christian Sack <christian.sack@outlook.com>
2022-07-29 15:54:56 +0200

    styles are switchable

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,26 +1,13 @@
-function burgerMenuClicked(sender){
-  sender.classList.toggle('active');
+function toggleMenu(){
+  [].forEach.call(document.querySelectorAll('.burger-menu'), el => {
+    el.classList.toggle('active');
+  })
   document.querySelector('#main-menu').classList.toggle('show');
 }
 
-// $(document).ready(function() {
-//   $('#header .holder .menu-icon .hj8l52-3').click(function() {
-//       $('#header .holder .menu-icon .bars').toggleClass('JGHjp');
-//       $('#header .holder .menu-icon .bar1').toggleClass('gMWLqb');
-//       $('#header .holder .menu-icon .bar2').toggleClass('hlQdsO');
-//       $('#header .holder .menu-icon .bar3').toggleClass('ezCxEq');
-//       $('#header .main-menu').toggleClass('open');
-//       $('#wrapper').toggleClass('open');
-//   });
-// });
-
-// $(document).ready(function () {
-//   $(".img-text .img-hold").hover(
-//     function () {
-//       $(this).addClass("box-open");
-//     },
-//     function () {
-//       $(this).removeClass("box-open");
-//     }
-//   );
-// });
+function toggleTheme(){
+  let darkStyleSheet = document.querySelector('#dark-stylesheet');
+  let lightStyleSheet = document.querySelector('#light-stylesheet');
+  darkStyleSheet.disabled = !darkStyleSheet.disabled;
+  lightStyleSheet.disabled = !lightStyleSheet.disabled;
+}
