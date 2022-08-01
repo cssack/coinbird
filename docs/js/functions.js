1390ffa97ae26d0aced0296b83bedee5e7a7277f
Christian Sack <christian.sack@outlook.com>
2022-08-01 09:12:04 +0200

    font is now swapable

--- docs/js/functions.js
+++ docs/js/functions.js
@@ -8,10 +8,13 @@ function toggleMenu(){
 
 // switches between a light theme and a dark theme
 function toggleTheme(){
-  let darkStyleSheet = document.querySelector('#dark-stylesheet');
-  let lightStyleSheet = document.querySelector('#light-stylesheet');
-  darkStyleSheet.disabled = !darkStyleSheet.disabled;
-  lightStyleSheet.disabled = !lightStyleSheet.disabled;
+  toggleCss('#dark-stylesheet')
+  toggleCss('#light-stylesheet')
+}
+
+function toggleCss(selector){
+  let stylesheet = document.querySelector(selector);
+  stylesheet.disabled = !stylesheet.disabled;
 }
 
 // here we add a css class to the body indicating if the mouse is left from the screen center or right.
@@ -19,7 +22,6 @@ function adaptMousePosition(evt, sender){
   document.body.classList.remove(evt.clientX < document.body.clientWidth/2 ? "mouse-right" : "mouse-left");
   document.body.classList.add(evt.clientX < document.body.clientWidth/2 ? "mouse-left" : "mouse-right");
 }
-
 
 // here we register an event handler registering whenever the mouse is over a explainable element.
 // whenever the mouse is over such an element, the explainer target will be updated to contain the explanation
